00c48f8a547951cc90c3a9c2a908910eddfbde44
fajarseptiawan <fajarsptiawan@gmail.com>
2026-03-03 10:57:34 +0800

    sl

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     @include('partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
-    @stack('jss')
+    @stack('js')
 </body>
 
 </html>
