0f2868416e1e0b4b3d053a6967e9d2a712ecc74c
fajarseptiawan <fajarsptiawan@gmail.com>
2026-03-03 20:11:24 +0800

    update logo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,9 +2,9 @@
     <div class="mx-auto w-full max-w-7xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
-                <a href="https://flowbite.com/" class="flex items-center">
-                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-7 me-3" alt="FlowBite Logo" />
-                    <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+                <a href="https://lfcatalog.jrsept.com" class="flex items-center">
+                    <img src="{{ asset('img/lfcatalog.png') }}" class="h-7 me-3" alt="LF_Catalog Logo" />
+                    <span class="text-heading self-center text-2xl font-semibold whitespace-nowrap">LF Catalog</span>
                 </a>
             </div>
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
