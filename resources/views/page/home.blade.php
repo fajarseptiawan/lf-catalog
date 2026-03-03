c2c1eeb0815d795c871f3066b026e6acf55b371f
fajarseptiawan <fajarsptiawan@gmail.com>
2026-03-03 15:25:40 +0800

    img up

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -10,27 +10,27 @@
         <div class="relative h-56 overflow-hidden  md:h-96">
             <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/tes2.png') }}"
+                <img src="{{ asset('img/sl1.jpg') }}"
                     class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 2 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/tes.png') }}"
+                <img src="{{ asset('img/sl2.jpg') }}"
                     class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 3 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/tes.png') }}"
+                <img src="{{ asset('img/sl3.jpg') }}"
                     class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 4 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/tes.png') }}"
+                <img src="{{ asset('img/sl4.jpg') }}"
                     class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
             <!-- Item 5 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/tes.png') }}"
+                <img src="{{ asset('img/sl2.jpg') }}"
                     class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
         </div>
